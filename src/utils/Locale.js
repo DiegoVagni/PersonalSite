0502a3117bcd16aedc4e8db08a1c24b4d8451f0d
IZANAGI\diego <diegomitro@hotmail.it>
2024-01-30 12:14:47 +0100

    splitted locale language and invariant labels

--- src/utils/Locale.js
+++ src/utils/Locale.js
@@ -1,14 +1,16 @@
-
+import InvariantLabels from "../resources/invariant_labels.json"
 class Locale {
 	static defaultLocale = localStorage["locale"] ? localStorage["locale"] : "it";
-	static localMap = null;
+	static invariantMap = null;
 	static localeList = [
 		{ name: "English", code: "en", lang: "English" },
 		{ name: "Italiano", code: "it", lang: "Italian" },
 	];
 
 	static ChangeLocale(localCode, refreshAppHook) {
-
+		if (this.invariantMap == null) {
+			this.invariantMap = new Map(Object.entries(InvariantLabels))
+		}
 		
 			Locale.defaultLocale = localCode;
 			fetch('Locals/' + localCode + '.json', {
@@ -22,6 +24,8 @@ class Locale {
 	}
 
 	static GetMessages(localName) {
+		if (this.invariantMap != null && this.invariantMap.get(localName)) return this.invariantMap.get(localName) 
+
 		if (this.localMap == null || this.localMap.get(localName) == undefined) return "MissingLocal";
 	
 		return this.localMap.get(localName);
