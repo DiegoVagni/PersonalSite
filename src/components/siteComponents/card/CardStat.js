c39b0372908894561ae8d5bb117c58087b8dc4f8
IZANAGI\diego <diegomitro@hotmail.it>
2024-02-02 11:37:51 +0100

    forgot to save cardstat

--- src/components/siteComponents/card/CardStat.js
+++ src/components/siteComponents/card/CardStat.js
@@ -3,25 +3,8 @@ import { Component } from "react"
 import StyleSheet from "../../../utils/StyleSheet"
 class CardStat extends Component {
 	render() {
-		let imgStyle = {
-			width: "50px",
-			height: "50px",
-			margin:"2px"
-		}
-		let divStyle = {
-			display: "flex",
-			flexDirection: "row",
-			justifyContent: "flex-start",
-			alignItems: "center",
-			justifyItems: "center",
-			margin:"5px"
-		}
-		let textStyle = {
-			font: "20px Tahoma, serif",
-			margin: "3px",
-			color:this.props.StyleSheet.Style("Text_Color")
-		}
-		return (<div style={divStyle}>
+	
+		return (<div style={StyleSheet.getLayoutStyle("Flex_Row_Center_Container")}>
 			<img style={StyleSheet.getLayoutStyle("Small_Image")} src={this.props.src} alt={this.props.title}>
 			</img>
 			<p style={StyleSheet.getLayoutStyle("Normal_Text")}>{this.props.title + ": "}</p>
